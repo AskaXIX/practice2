fa840c2a90dc8727fad46220760d241ea8d085f4
Aslan <aslan.aslan.14@bk.ru>
2023-07-21 20:32:43 +0600

    task1 done

---
+++ task1.py
@@ -0,0 +1,26 @@
+# На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом. 
+# Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты вверх одной и той же стороной. 
+# Выведите минимальное количество монет, которые нужно перевернуть
+
+from random import randint
+
+n = int(input("n: "))
+coins = []
+
+for i in range(n):
+    coins.append(randint(0, 1))
+print(coins)
+coins_0 = coins.count(0)
+coins_1 = coins.count(1)
+# for ele in coins:
+#     if coins[index] == 0:
+#         coins_0 += 1
+#         index += 1
+#     elif coins[index] == 1:
+#         coins_1 += 1
+#         index += 1
+
+if coins_0 > coins_1:
+    print(f"\n{coins_1}")
+else:
+    print(f"\n{coins_0}")
